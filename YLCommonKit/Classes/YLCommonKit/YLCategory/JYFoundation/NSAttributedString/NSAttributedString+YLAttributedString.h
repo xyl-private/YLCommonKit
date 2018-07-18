73fb5ec394f5531aeafeb6cdaf61f4c0082131d2
徐彦龙 <yanlongxu@jieyuechina.com>
2018-07-18 10:38:32 +0800

    添加 NSAttributedString

---
+++ YLCommonKit/Classes/YLCommonKit/YLCategory/JYFoundation/NSAttributedString/NSAttributedString+YLAttributedString.h
@@ -0,0 +1,27 @@
+//
+//  NSAttributedString+YLAttributedString.h
+//  YLCommonKit
+//
+//  Created by xyanl on 2018/7/18.
+//  Copyright © 2018年 xyanl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NSAttributedString (YLAttributedString)
+
+/**
+ * 设置段落样式
+ */
++ (NSAttributedString *)yl_stringWithParagraphlineSpeace:(CGFloat)lineSpacing kernAttribute:(CGFloat)kernAttribute textColor:(UIColor *)textcolor textFont:(UIFont *)font string:(NSString *)string;
+/**
+ *  计算富文本字体高度
+ */
++ (CGFloat)yl_getSpaceLabelHeightwithSpeace:(CGFloat)lineSpeace kernAttribute:(CGFloat)kernAttribute withFont:(UIFont *)font withWidth:(CGFloat)width string:(NSString *)string;
+/**
+ *  计算富文本字体宽度
+ */
++ (CGFloat)yl_getSpaceLabelWidthWithSpeace:(CGFloat)lineSpeace kernAttribute:(CGFloat)kernAttribute font:(UIFont *)font height:(CGFloat)height string:(NSString *)string;
+
+@end
