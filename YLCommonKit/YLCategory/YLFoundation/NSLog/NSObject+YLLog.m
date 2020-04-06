63bf20a18f1d0c0b33c39df67c3090cb2d392261
xyanl <xyl_private@163.com>
2020-04-07 02:21:13 +0800

    优化项目

---
+++ YLCommonKit/YLCategory/YLFoundation/NSLog/NSObject+YLLog.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+YLLog.m
+//  JBStore
+//
+//  Created by xyanl on 2020/4/7.
+//  Copyright © 2020 xyanl. All rights reserved.
+//
+
+#import "NSObject+YLLog.h"
+
+@implementation NSObject (YLLog)
+
+@end
