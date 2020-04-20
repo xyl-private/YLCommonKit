0fa7acb18f84a6634b9de984fa6e54d64b4e8d21
xyanl <xyl_private@163.com>
2020-04-20 21:32:17 +0800

    优化渐变色

--- YLCommonKit/YLCategory/YLUIKit/UIColor/UIColor+YLColor.h
+++ YLCommonKit/YLCategory/YLUIKit/UIColor/UIColor+YLColor.h
@@ -10,27 +10,42 @@
 
 @interface UIColor (YLColor)
 
-/**
- *  十六进制颜色 - 不透明
- */
+/// 十六进制颜色 - 不透明
 + (UIColor *) yl_colorWithHexString:(NSString *)hexColor;
 
-/**
- *  十六进制颜色 - 可透明
- */
+/// 十六进制颜色 - 可透明
 + (UIColor *) yl_colorWithHexString:(NSString *)hexColor alpha:(CGFloat)alpha;
-
-/**
- 获取渐变色Layer
- 
- @param colors 颜色值 [UIcolor]
- @param locations 渐变色位置 总长度为1
- @param frame frame
- @return CAGradientLayer
- */
-+ (CAGradientLayer *) yl_getGraduallyChangingColor:(NSArray *)colors locations:(NSArray *)locations frame:(CGRect)frame;
 
 /// 随机色
 + (UIColor *)yl_randomColor;
+
+
+/**
+*  设置渐变色 默认从左向右 横向渐变
+*  @param colors 颜色数组,
+*  例:@[[UIColor redColor],[UIColor blackColor]] 或者
+*  @[(__bridge id)[UIColor redColor].CGColor,(__bridge id)[UIColor blackColor].CGColor] 两者都可以
+*  @param locations 渐变色区间,0~1,可为空,默认平均分配
+*  @param frame view.bouns
+*/
++ (CAGradientLayer *)yl_setGradientLayerVerticallyWithColors:(NSArray *)colors
+                                                   locations:(NSArray *)locations
+                                                       frame:(CGRect)frame;
+
+/**
+ *  设置渐变色
+ *  @param colors 颜色数组,
+ *  例:@[[UIColor redColor],[UIColor blackColor]] 或者
+ *  @[(__bridge id)[UIColor redColor].CGColor,(__bridge id)[UIColor blackColor].CGColor] 两者都可以
+ *  @param locations 渐变色区间,0~1,可为空,默认平均分配
+ *  @param frame view.bouns
+ *  @param startPoint  起始位置坐标 以左上角为起始位置 坐标(0,0) 右下角坐标(1,1)
+ *  @param endPoint 终止位置坐标
+ */
++ (CAGradientLayer *)yl_setGradientLayerWithColors:(NSArray *)colors
+                                         locations:(NSArray *)locations
+                                             frame:(CGRect)frame
+                                        startPoint:(CGPoint)startPoint
+                                          endPoint:(CGPoint)endPoint;
 
 @end
--- YLCommonKit/YLCategory/YLUIKit/UIColor/UIColor+YLColor.m
+++ YLCommonKit/YLCategory/YLUIKit/UIColor/UIColor+YLColor.m
@@ -59,19 +59,61 @@ static NSString * const colorStrPrefix2 = @"#";
     return [UIColor colorWithRed:((CGFloat)red/ 255.0f) green:((CGFloat)green/ 255.0f) blue:((CGFloat)blue/ 255.0f) alpha:alpha];
 }
 
-+ (CAGradientLayer *) yl_getGraduallyChangingColor:(NSArray *)colors locations:(NSArray *)locations frame:(CGRect)frame{
++ (UIColor *)yl_randomColor{
+    CGFloat red = random()%255/255.0;
+    CGFloat green = random()%255/255.0;
+    CGFloat blue = random()%255/255.0;
+   return [UIColor colorWithRed:red green:green blue:blue alpha:1];
+}
+
+/**
+*  设置渐变色 默认从左向右 横向渐变
+*  @param colors 颜色数组,
+*  例:@[[UIColor redColor],[UIColor blackColor]] 或者
+*  @[(__bridge id)[UIColor redColor].CGColor,(__bridge id)[UIColor blackColor].CGColor] 两者都可以
+*  @param locations 渐变色区间,可为空,默认平均分配
+*  @param frame view.bouns
+*/
++ (CAGradientLayer *)yl_setGradientLayerVerticallyWithColors:(NSArray *)colors
+                                                   locations:(NSArray *)locations
+                                                       frame:(CGRect)frame
+{
+    return [UIColor yl_setGradientLayerWithColors:colors locations:locations frame:frame startPoint:CGPointMake(0, 0) endPoint:CGPointMake(1.0, 0)];
+}
+
+/**
+ *  设置渐变色
+ *  @param colors 颜色数组,
+ *  例:@[[UIColor redColor],[UIColor blackColor]] 或者
+ *  @[(__bridge id)[UIColor redColor].CGColor,(__bridge id)[UIColor blackColor].CGColor] 两者都可以
+ *  @param locations 渐变色区间,可为空,默认平均分配
+ *  @param frame view.bouns
+ *  @param startPoint  起始位置坐标 以左上角为起始位置 坐标(0,0) 右下角坐标(1,1)
+ *  @param endPoint 终止位置坐标
+ */
++ (CAGradientLayer *)yl_setGradientLayerWithColors:(NSArray *)colors
+                                         locations:(NSArray *)locations
+                                             frame:(CGRect)frame
+                                        startPoint:(CGPoint)startPoint
+                                          endPoint:(CGPoint)endPoint
+{
     CAGradientLayer *gradientLayer = [CAGradientLayer layer];
-    NSMutableArray *CGColorsArr = [NSMutableArray array];
+    NSMutableArray *colorsMarr = [NSMutableArray array];
     for (int i = 0 ; i<colors.count; i++) {
         id obj = colors[i];
-        if ([[obj class] isKindOfClass:[UIColor class]]) {
-            UIColor *color = (UIColor *)obj;
-            [CGColorsArr addObject:color];
+        //获得色值描述
+        NSString * describe = [NSString stringWithFormat:@"%@",obj];
+        NSArray * arr = [describe componentsSeparatedByString:@" "];
+        NSString * colorDescribe = arr.firstObject;
+        if ([colorDescribe isEqualToString:@"UIExtendedSRGBColorSpace"] ||
+            [colorDescribe isEqualToString:@"UICachedDeviceRGBColor"] ) {
+            UIColor * c = (UIColor *)obj;
+            [colorsMarr addObject:(__bridge id)c.CGColor];
         }else{
-            [CGColorsArr addObject:obj];
+            [colorsMarr addObject:obj];
         }
     }
-    gradientLayer.colors = CGColorsArr;
+    gradientLayer.colors = colorsMarr;
     
     if (locations.count) {
         gradientLayer.locations = locations;
@@ -83,16 +125,9 @@ static NSString * const colorStrPrefix2 = @"#";
         }
         gradientLayer.locations = templocations;
     }
-    gradientLayer.startPoint = CGPointMake(0, 0);
-    gradientLayer.endPoint = CGPointMake(1.0, 0);
+    gradientLayer.startPoint = startPoint;
+    gradientLayer.endPoint = endPoint;
     gradientLayer.frame = frame;
     return gradientLayer;
-}
-
-+ (UIColor *)yl_randomColor{
-    CGFloat red = random()%255/255.0;
-    CGFloat green = random()%255/255.0;
-    CGFloat blue = random()%255/255.0;
-   return [UIColor colorWithRed:red green:green blue:blue alpha:1];
 }
 @end
